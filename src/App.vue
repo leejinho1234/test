a1cadca49be0fb590358a7ec872e7a56b0e6c6e0
leejinho1234 <ever1479@gmail.com>
2020-01-08 00:39:20 +0900

    2020/01/08

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-card flat>
+    <v-toolbar height="100" style="border-bottom: 1px solid #E3DFDE" flat>
+    <v-spacer></v-spacer>
+    <div style="color:orange; cursor:pointer">LOGIN</div>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-toolbar-title><img src="https://ifh.cc/g/ywJDY.png" width="200"></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <div>JOIN</div>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+  <v-toolbar flat style="border-bottom: 1px solid #E3DFDE" height="50">
+    <v-tabs grow centered>
+      <v-tabs-slider color="yellow"></v-tabs-slider>
+      <v-tab v-for="bar in title" :key="bar.title">{{ bar.titleName }}</v-tab>
+  </v-tabs>
+  </v-toolbar>
+  <v-carousel cycle hide-delimiters>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      :adjustableHeightEasing="true"
+    >
+    <v-layout align-center>
+    <v-flex text-xs-center text-sm-left class="box"><div>{{ item.text }}</div></v-flex>
+    </v-layout>
+    </v-carousel-item>
+  </v-carousel>
+  </v-card>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: 'https://ifh.cc/g/olWeL.jpg',
+          text: 'gdgd'
+        },
+        {
+          src: 'https://ifh.cc/g/3QMao.jpg',
+          text: 'gdasdd'
+        }
+      ],
+      title: [
+        {
+          titleName: 'EVENT'
+        },
+        {
+          titleName: 'STORE'
+        },
+        {
+          titleName: 'EDUCATION'
+        },
+        {
+          titleName: 'Q&A'
+        },
+        {
+          titleName: 'HELP'
+        },
+        {
+          titleName: 'JJJ'
+        }
+      ]
     }
   }
+}
+
+</script>
+<style>
+.box {
+  display: block;
+  padding: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueCarousel from 'vue-carousel'
 // import test from './plugins/test'
-
 Vue.config.productionTip = false
-
+Vue.use(VueCarousel)
 // test.foo()
 
 new Vue({
   router,
   store,
   vuetify,
+  VueCarousel,
   render: h => h(App)
 }).$mount('#app')
